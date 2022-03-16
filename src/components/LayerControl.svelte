7f76e1807aa5430a133bece4e9452c28a37f3f56
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-03-16 07:28:54 -0500

    make geojson work with LayerControl
    - enable custom style
    - enable custom popup

--- src/components/LayerControl.svelte
+++ src/components/LayerControl.svelte
@@ -5,7 +5,7 @@
   const { getMap } = getContext(L);
 
   export let baseLayersData = {};
-  export let overlayLayersData = {};
+  export let overlayLayersData = [];
 
   let baseLayers = {};
   let overlayLayers = {};
@@ -19,8 +19,42 @@
     }
   });
 
-  Object.entries(overlayLayersData).forEach(([key, value], index) => {
-    overlayLayersData[key] = L.tileLayer(value.url, value.options);
+  function onEachFeature(feature, layer, overlay) {
+    if (feature.properties) {
+      let popupContent = '';
+      if (overlay.popup_fields) {
+        popupContent += '<table class="popup-table not-prose">\n';
+        overlay.popup_fields.forEach((field) => {
+          popupContent += '<tr>\n';
+          popupContent += '<th>' + field.name + '</th>';
+          popupContent += '<td>' + feature.properties[field.original_name] + '</td>';
+          popupContent += '</tr>\n';
+        });
+        popupContent += '</table>';
+      } else {
+        popupContent = JSON.stringify(feature.properties);
+      }
+      layer.bindPopup(popupContent);
+    }
+  }
+
+  overlayLayersData.forEach((overlay) => {
+    let layer;
+    let options = {
+      ...overlay.options,
+      onEachFeature: (feature, layer) => onEachFeature(feature, layer, overlay),
+      style: function (feature) {
+        return {
+          color: feature.properties.color,
+          opacity: overlay.opacity || 1,
+          fillOpacity: overlay.fillOpacity || 0.2
+        };
+      }
+    };
+    if (overlay.type === 'geojson') {
+      layer = L.geoJSON(overlay.data, options);
+      overlayLayers[overlay.name] = layer;
+    }
   });
 
   let layerControl;
